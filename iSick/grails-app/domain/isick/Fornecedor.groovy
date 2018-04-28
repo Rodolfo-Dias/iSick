fd6529de878df7527347813290902ae1d902a3f3
HenriqueKajihara <HenriqueKajihara@HenriqueKajihara>
2018-04-28 16:40:56 -0300

    Dominio fornecedor

---
+++ iSick/grails-app/domain/isick/Fornecedor.groovy
@@ -0,0 +1,24 @@
+package isick
+
+class Fornecedor {
+	String cnpj	 
+	String razaoSocial
+	String nomeFantasia
+	String inscricaoEstadual	 
+	String endereco	 
+	String municipio	 
+	String bairro	 
+	int cep	 
+	String telefone	
+    static constraints = {
+        cnpj size: 11..14, blank:false, unique: true
+        razaoSocial blank: false
+	nomeFantasia blank: false
+	inscricaoEstadual blank: false
+	endereco size: 1..30, blank:false
+	municipio size: 1..30, blank:false
+	bairro size: 1..30, blank:false
+	cep size:8..9, blank: false
+	telefone blank: false
+    }
+}
